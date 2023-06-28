a71d6d299d1d131c9de40133da670d93911bacf9
MinnerGun <94054191+MinnerGun@users.noreply.github.com>
2023-06-28 22:17:04 +0700

    Added User update

--- user_header.php
+++ user_header.php
@@ -50,7 +50,7 @@
             $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
          ?>
          <p><?= $fetch_profile["name"]; ?></p>
-         <a href="update_user.php" class="btn">update profile</a>
+         <a href="user_update.php" class="btn">update profile</a>
          <div class="flex-btn">
             <a href="user_register.php" class="option-btn">register</a>
             <a href="user_login.php" class="option-btn">login</a>
