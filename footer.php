7db2ace02f1b87f332384b64f550c117c26eebf1
MinnerGun <94054191+MinnerGun@users.noreply.github.com>
2023-07-06 21:04:18 +0700

    Added placed order history and fixes footer bug

--- footer.php
+++ footer.php
@@ -23,15 +23,15 @@
          <a href="tel:628111222333"><i class="fas fa-phone"></i> +628 111 222 333</a>
          <a href="tel:628444555666"><i class="fas fa-phone"></i> +628 444 555 666</a>
          <a href="mailto:ilikepancake387@gmail.com"><i class="fas fa-envelope"></i> myemail@gmail.com</a>
-         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> Batam, Indonesia - 400104 </a>
+         <a href="https://www.google.com/maps/d/u/0/edit?mid=18gx3jGCUZfzcI3Fhv15VASfOJ4qAXg4&usp=sharing"><i class="fas fa-map-marker-alt"></i> Batam, Indonesia - 400104 </a>
       </div>
 
       <div class="box">
          <h3>follow us</h3>
-         <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
-         <a href="#"><i class="fab fa-twitter"></i>twitter</a>
-         <a href="#"><i class="fab fa-instagram"></i>instagram</a>
-         <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+         <a href="https://www.facebook.com"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="https://twitter.com/imaweebs"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="https://www.instagram.com/vincnt31/"><i class="fab fa-instagram"></i>instagram</a>
+         <a href="https://www.linkedin.com"><i class="fab fa-linkedin"></i>linkedin</a>
       </div>
 
    </section>
